14d217e65cc61d619e1923f4d8db1d3c5435106e
KUSHAGRA SHUKLA <shukla.kushagra.business@gmail.com>
2022-02-20 15:37:59 +0530

    102. Medium

---
+++ Leetcode medium/Binary_tree_level_order_traversal.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+public:
+    vector<vector<int>> levelOrder(TreeNode* root)
+    {
+        vector<vector<int>>result;
+        if(root==NULL)
+            return result;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size=q.size();
+            cout<<size;
+            vector<int>ans;
+            while(size--)
+            {
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left!=NULL)
+                    q.push(node->left);
+                if(node->right!=NULL)
+                    q.push(node->right);
+                ans.push_back(node->val);
+            }
+            result.push_back(ans);
+        }
+    return result;
+}
