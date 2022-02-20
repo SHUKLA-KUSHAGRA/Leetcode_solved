a6ed3fe2995c2d193e2c9728bdc4594a45361f03
KUSHAGRA SHUKLA <shukla.kushagra.business@gmail.com>
2022-02-20 13:27:10 +0530

    13. Easy

---
+++ Leetcode Easy/Roman _to_integer.cpp
@@ -0,0 +1,17 @@
+int romanToInt(string s)
+{
+   unordered_map<char,int> t = { { 'I' , 1 },{ 'V' , 5 },{ 'X' , 10 },{ 'L' , 50 },{ 'C' , 100 },{ 'D' , 500 },{ 'M' , 1000 } };
+   int sum = t[s.back()];
+   for (int i=s.length()-2;i>=0;--i) 
+   {
+       if(t[s[i]]<t[s[i + 1]])
+       {
+           sum=sum-T[s[i]];
+       }
+       else
+       {
+           sum =sum+t[s[i]];
+       }
+   }
+   return sum;
+}
