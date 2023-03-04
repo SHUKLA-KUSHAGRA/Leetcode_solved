038d6d2534356c52ee9fbde17d2deaff100b6879
KUSHAGRA SHUKLA <shukla.kushagra.business@gmail.com>
2023-03-05 00:53:25 +0530

    Added solution - LeetHub

--- Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
+++ Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
@@ -13,22 +13,25 @@ class Solution {
     int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
                      pair<int, int> destination) {
         // code here
+        // DIJKSTRA ALGORITHM
         int n=grid.size();
         int m=grid[0].size();
-        int X=destination.first;
-        int Y=destination.second;
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>>q;
-        q.push({{source.first,source.second},0});
-        vis[source.first][source.second]=1;
-        while(q.size()!=0)
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        int sx=source.first;
+        int sy=source.second;
+        int fx=destination.first;
+        int fy=destination.second;
+        pq.push({0,{sx,sy}});
+        dist[sx][sy]=0;
+        while(pq.size()!=0)
         {
-            pair<pair<int,int>,int>p=q.front();
-            q.pop();
-            int x=p.first.first;
-            int y=p.first.second;
-            int d=p.second;
-            if(x==X && y==Y)
+            pair<int,pair<int,int>>p=pq.top();
+            pq.pop();
+            int d=p.first;
+            int x=p.second.first;
+            int y=p.second.second;
+            if(x==fx && y==fy)
                 return d;
             for(int delx=-1;delx<=1;delx++)
             {
@@ -38,12 +41,12 @@ class Solution {
                     int ny=y+dely;
                     if(delx==0 || dely==0)
                     {
-                        if(nx>=0 && nx<n && ny>=0 && ny<m)
+                        if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny]==1)
                         {
-                            if(grid[nx][ny]==1 && vis[nx][ny]==0)
+                            if(dist[x][y]+1<dist[nx][ny])
                             {
-                                vis[nx][ny]=1;
-                                q.push({{nx,ny},d+1});
+                                dist[nx][ny]=dist[x][y]+1;
+                                pq.push({d+1,{nx,ny}});
                             }
                         }
                     }
@@ -52,6 +55,46 @@ class Solution {
         }
         return -1;
     }
+        // BFS ALGORITHM
+    //     int n=grid.size();
+    //     int m=grid[0].size();
+    //     int X=destination.first;
+    //     int Y=destination.second;
+    //     vector<vector<int>>vis(n,vector<int>(m,0));
+    //     queue<pair<pair<int,int>,int>>q;
+    //     q.push({{source.first,source.second},0});
+    //     vis[source.first][source.second]=1;
+    //     while(q.size()!=0)
+    //     {
+    //         pair<pair<int,int>,int>p=q.front();
+    //         q.pop();
+    //         int x=p.first.first;
+    //         int y=p.first.second;
+    //         int d=p.second;
+    //         if(x==X && y==Y)
+    //             return d;
+    //         for(int delx=-1;delx<=1;delx++)
+    //         {
+    //             for(int dely=-1;dely<=1;dely++)
+    //             {
+    //                 int nx=x+delx;
+    //                 int ny=y+dely;
+    //                 if(delx==0 || dely==0)
+    //                 {
+    //                     if(nx>=0 && nx<n && ny>=0 && ny<m)
+    //                     {
+    //                         if(grid[nx][ny]==1 && vis[nx][ny]==0)
+    //                         {
+    //                             vis[nx][ny]=1;
+    //                             q.push({{nx,ny},d+1});
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return -1;
+    // }
 };
 
 
